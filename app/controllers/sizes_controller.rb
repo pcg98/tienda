c07e55652da790fe20ef94784a72ffb0b5acddc0
Pablo <pablo_cg_baeza@hotmail.com>
2022-06-02 13:02:30 +0200

    Se pueden insertar tallas, borrar, mostrar y las que tienen stock 0 no aparecen

---
+++ app/controllers/sizes_controller.rb
@@ -0,0 +1,26 @@
+class SizesController < ApplicationController
+  def create
+    #Pillamos producto
+    @producto = Producto.find(params[:producto_id])
+    #En ese producto, en las tallas creamos la nueva
+    @size = @producto.sizes.create(size_params)
+    redirect_to producto_path(@producto)
+  end
+
+  def destroy
+    #Encontramos producto
+    @producto = Producto.find(params[:producto_id])
+    #Encontramos talla
+    @size = @producto.sizes.find(params[:id])
+    #Destruimos
+    @size.destroy
+    redirect_to producto_path(@producto), status: 303
+  end
+
+
+  private
+  #Que atributos necesitamos de size
+  def size_params
+    params.require(:size).permit(:stock,:talla)
+  end
+end
